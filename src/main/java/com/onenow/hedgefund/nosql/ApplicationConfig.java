d8d3b873df1508fa34b5fab7cac308118afc1992
Yougeshwar <ypkkhatri@gmail.com>
2016-09-29 23:44:05 +0500

    pom file revert

--- src/main/java/com/onenow/hedgefund/nosql/ApplicationConfig.java
+++ src/main/java/com/onenow/hedgefund/nosql/ApplicationConfig.java
@@ -1,9 +1,5 @@
 package com.onenow.hedgefund.nosql;
 
-import io.swagger.jaxrs.listing.ApiListingResource;
-import io.swagger.jaxrs.listing.SwaggerSerializers;
-import org.glassfish.jersey.jackson.JacksonFeature;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.Collections;
@@ -21,10 +17,10 @@ public class ApplicationConfig extends Application
     public Set<Class<?>> getClasses()
     {
         Set<Class<?>> resources = new java.util.HashSet<>();
-        resources.add(JacksonFeature.class);
-        resources.add(NoSqlEndpoint.class);
-        resources.add(ApiListingResource.class);
-        resources.add(SwaggerSerializers.class);
+//        resources.add(JacksonFeature.class);
+//        resources.add(NoSqlEndpoint.class);
+//        resources.add(ApiListingResource.class);
+//        resources.add(SwaggerSerializers.class);
 
         return Collections.unmodifiableSet(resources);
     }
