e09f54e5601379912ec5e01b1630a6d575f522ea
pablo <pablo.personal@icloud.com>
2016-09-29 14:01:42 -0700

    commented methods to narrow down where the error could come from

--- src/main/java/com/onenow/hedgefund/nosqlrest/NoSqlEndpoint.java
+++ src/main/java/com/onenow/hedgefund/nosqlrest/NoSqlEndpoint.java
@@ -25,42 +25,42 @@ public class NoSqlEndpoint
 //        }
 //    }
 
-    @POST
-    @Path("/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response POST(Model model)
-    {
-        try
-        {
-            NoSqlService.POST(model.getLookup(), model.getItemJson(), model.getTableName());
-            return Response.ok().build();
-        }
-        catch (Exception ex)
-        {
-            Watchr.log(ExceptionUtil.exceptionToString(ex));
-            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
-        }
-    }
-
-    @PUT
-    @Path("/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response PUT(Model model)
-    {
-        try
-        {
-            NoSqlService.PUT(model.getLookup(), model.getItemJson(), model.getTableName());
-            return Response.ok().build();
-        }
-        catch (Exception ex)
-        {
-            Watchr.log(ExceptionUtil.exceptionToString(ex));
-            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
-        }
-    }
+//    @POST
+//    @Path("/")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response POST(Model model)
+//    {
+//        try
+//        {
+//            NoSqlService.POST(model.getLookup(), model.getItemJson(), model.getTableName());
+//            return Response.ok().build();
+//        }
+//        catch (Exception ex)
+//        {
+//            Watchr.log(ExceptionUtil.exceptionToString(ex));
+//            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
+//        }
+//    }
+//
+//    @PUT
+//    @Path("/")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response PUT(Model model)
+//    {
+//        try
+//        {
+//            NoSqlService.PUT(model.getLookup(), model.getItemJson(), model.getTableName());
+//            return Response.ok().build();
+//        }
+//        catch (Exception ex)
+//        {
+//            Watchr.log(ExceptionUtil.exceptionToString(ex));
+//            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
+//        }
+//    }
 
     @GET
-    @Path("/")
+//    @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public static Response GET() {
         try
@@ -74,86 +74,86 @@ public class NoSqlEndpoint
         }
     }
 
-    @GET
-    @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response GET(@QueryParam("tableName") String tableName)
-    {
-
-        try
-        {
-            return Response.ok(Piping.serialize(NoSqlService.GET(tableName))).build();
-        }
-        catch (Exception ex)
-        {
-            Watchr.log(ExceptionUtil.exceptionToString(ex));
-            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
-        }
-    }
-
-    @GET
-    @Path("/{ID}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response GET(@PathParam("ID") String lookup,
-                        @QueryParam("tableName") String tableName)
-    {
-        Watchr.log("LOOKING UP " + lookup);
-        try
-        {
-            return Response.ok(Piping.serialize(NoSqlService.GET(lookup, tableName))).build();
-        }
-        catch (Exception ex)
-        {
-            Watchr.log(ExceptionUtil.exceptionToString(ex));
-            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
-        }
-    }
-
-    @GET
-    @Path("/query")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response GET(@QueryParam("fromDate") String fromDate,
-                        @QueryParam("toDate") String toDate,
-                        @QueryParam("dateFormat") String dateFormat,
-                        @QueryParam("timeZone") String timeZone,
-                        @QueryParam("tableName") String tableName)
-    {
-
-        // String log = "INPUT: " + fromDate + " " + toDate + " " + dateFormat + " " + timeZone;
-        // Watchr.log(log);
-
-        // TODO LATER: date format change if necessary... dateFormat, timeZone
-
-        try
-        {
-            DynamoResponse response = NoSqlService.GET(fromDate, toDate, tableName);
-            String serialized = Piping.serialize(response);
-            return Response.ok(serialized).build();
-        }
-        catch (Exception ex)
-        {
-            Watchr.log(ExceptionUtil.exceptionToString(ex));
-            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
-        }
-    }
-
-    @DELETE
-    @Path("/{ID}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response DELETE(@PathParam("ID") String lookup,
-                           @QueryParam("tableName") String tableName)
-    {
-
-        try
-        {
-            NoSqlService.DELETE(lookup, tableName);
-            return Response.ok().build();
-        }
-        catch (Exception ex)
-        {
-            Watchr.log(ExceptionUtil.exceptionToString(ex));
-            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
-        }
-    }
+//    @GET
+//    @Path("/")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response GET(@QueryParam("tableName") String tableName)
+//    {
+//
+//        try
+//        {
+//            return Response.ok(Piping.serialize(NoSqlService.GET(tableName))).build();
+//        }
+//        catch (Exception ex)
+//        {
+//            Watchr.log(ExceptionUtil.exceptionToString(ex));
+//            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
+//        }
+//    }
+//
+//    @GET
+//    @Path("/{ID}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response GET(@PathParam("ID") String lookup,
+//                        @QueryParam("tableName") String tableName)
+//    {
+//        Watchr.log("LOOKING UP " + lookup);
+//        try
+//        {
+//            return Response.ok(Piping.serialize(NoSqlService.GET(lookup, tableName))).build();
+//        }
+//        catch (Exception ex)
+//        {
+//            Watchr.log(ExceptionUtil.exceptionToString(ex));
+//            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
+//        }
+//    }
+//
+//    @GET
+//    @Path("/query")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response GET(@QueryParam("fromDate") String fromDate,
+//                        @QueryParam("toDate") String toDate,
+//                        @QueryParam("dateFormat") String dateFormat,
+//                        @QueryParam("timeZone") String timeZone,
+//                        @QueryParam("tableName") String tableName)
+//    {
+//
+//        // String log = "INPUT: " + fromDate + " " + toDate + " " + dateFormat + " " + timeZone;
+//        // Watchr.log(log);
+//
+//        // TODO LATER: date format change if necessary... dateFormat, timeZone
+//
+//        try
+//        {
+//            DynamoResponse response = NoSqlService.GET(fromDate, toDate, tableName);
+//            String serialized = Piping.serialize(response);
+//            return Response.ok(serialized).build();
+//        }
+//        catch (Exception ex)
+//        {
+//            Watchr.log(ExceptionUtil.exceptionToString(ex));
+//            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
+//        }
+//    }
+//
+//    @DELETE
+//    @Path("/{ID}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response DELETE(@PathParam("ID") String lookup,
+//                           @QueryParam("tableName") String tableName)
+//    {
+//
+//        try
+//        {
+//            NoSqlService.DELETE(lookup, tableName);
+//            return Response.ok().build();
+//        }
+//        catch (Exception ex)
+//        {
+//            Watchr.log(ExceptionUtil.exceptionToString(ex));
+//            return Response.status(Response.Status.BAD_REQUEST).entity(ExceptionUtil.exceptionToString(ex)).build();
+//        }
+//    }
 
 }
