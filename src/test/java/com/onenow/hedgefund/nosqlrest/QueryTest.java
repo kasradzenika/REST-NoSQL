2f9059e223f1d88f431a474df15b15beb18222f0
Yogueshwar Khatri <ypkkhatri@gmail.com>
2017-01-10 09:57:22 +0500

    Test case changes

--- src/test/java/com/onenow/hedgefund/nosqlrest/QueryTest.java
+++ src/test/java/com/onenow/hedgefund/nosqlrest/QueryTest.java
@@ -11,24 +11,25 @@ import org.testng.annotations.Test;
  */
 public class QueryTest {
 
-//    @Test
-//    public void getContractsInDateRange()
-//    {
-//        DynamoResponse response = null;
-//        try
-//        {
-//            response = NoSqlService.GET(TableName.CONTRACTS.toString(),
-//                                        "2016-01-01 00:00:00",
-//                                        "2016-12-31 23:59:59",
-//                                        "yyyy-MM-dd hh:mm:ss",
-//                                        "UTC");
-//            Watchr.log("RESPONSE " + response);
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
+    @Test
+    public void getContractsInDateRange()
+    {
+        DynamoResponse response = null;
+        try
+        {
+            response = NoSqlService.GET(TableName.CONTRACTS.toString(),
+                                        "2016-01-01 00:00:00",
+                                        "2016-12-31 23:59:59",
+                                        "yyyy-MM-dd hh:mm:ss",
+                                        "UTC");
+            Watchr.log("RESPONSE: " + response);
+            Watchr.log("RESPONSE: " + (response != null ? response.resources : "NULL"));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 //        Assert.assertTrue(response != null);
 //        Assert.assertTrue(!response.resources.get(0).item.equals(""));
-//    }
+    }
 }
